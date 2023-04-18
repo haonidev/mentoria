115b50012d4e1ae6876c9cb37145abf25cd1f01c
Haoni Hashimoto <hashimoto@Haonis-MacBook-Pro.local>
2023-04-18 11:46:36 -0300

    Criacao da arena

--- strategy_example/Exercicio/arena.py
+++ strategy_example/Exercicio/arena.py
@@ -10,5 +10,39 @@ class Arena():
         self.robo_azul = robo_azul
 
 
-    def fight():
+    def temos_um_vencedor(self):
+        if self.robo_vermelho.hitpont <= 0:
+            print("O robo azul venceu a luta")
+            return True
+        elif self.robo_azul.hitpont <= 0:
+            print("O robo vermelho venceu a luta")
+            return True
+        
+        return False
+
+    def fight(self):
+        while True:
+            self.robo_vermelho.bater_com_braco_esquerdo(self.robo_azul)
+            self.robo_azul.bater_com_braco_esquerdo(self.robo_vermelho)
+            if self.temos_um_vencedor():
+                break
+
+            self.robo_vermelho.bater_com_braco_direito(self.robo_azul)
+            self.robo_azul.bater_com_braco_direito(self.robo_vermelho)
+            if self.temos_um_vencedor():
+                break
+
+            self.robo_vermelho.chutar_com_perna_direita(self.robo_azul)
+            self.robo_azul.chutar_com_perna_direita(self.robo_vermelho)
+            if self.temos_um_vencedor():
+                break
+
+            self.robo_vermelho.chutar_com_perna_esquerda(self.robo_azul)
+            self.robo_azul.chutar_com_perna_esquerda(self.robo_vermelho)
+            if self.temos_um_vencedor():
+                break
+
+            print(f"O robo vermelho tem {self.robo_vermelho.hitpont} de vida")
+            print(f"O robo azul tem {self.robo_azul.hitpont} de vida")
+
 
