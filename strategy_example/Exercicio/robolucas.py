a0cb53f7b60bf392c23eec066d3c70d094826766
Lucas Bianchi <lucas_bianchi123@hotmail.com>
2023-04-19 10:44:28 -0400

    agora vai

--- strategy_example/Exercicio/robolucas.py
+++ strategy_example/Exercicio/robolucas.py
@@ -1,13 +1,16 @@
+from robo import BaseRobo
+
+
 class RoboLucas(BaseRobo):
-    
+
     def __init__(self, Modelo: str, braco_esquerdo: any, braco_direito: any, perna_esquerda: any, perna_direita: any):
         self.braco_esquerdo = braco_esquerdo
         self.braco_direito = braco_direito
         self.perna_esquerda = perna_esquerda
         self.perna_direita = perna_direita
-    
+
         self.hitpoint = 10
-  
+
     def bater_com_braco_esquerdo(self, robo):
         print("Soco com o braco esquerdo")
         robo.hitpoint -= 3
@@ -16,13 +19,16 @@ class RoboLucas(BaseRobo):
         print("Soco com o braco direito")
         robo.hitpoint -= 3
 
-    def bater_com_perna_esquerda(self, robo):
+    def chutar_com_perna_esquerda(self, robo):
         print("Soco com o perna esquerda")
         robo.hitpoint -= 2
 
-    def bater_com_perna_direita(self, robo):
+    def chutar_com_perna_direita(self, robo):
         print("Soco com o perna direita")
         robo.hitpoint -= 2
 
+
 # Hora da porrada
-robo_lucas = RoboLucas(Modelo = "Megazord")
+if __name__ == "__main__":
+
+    robo_lucas = RoboLucas("Megazord", None, None, None, None)
